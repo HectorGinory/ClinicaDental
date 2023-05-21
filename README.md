6ae43483aa7c4fca03dce1e12062acfff26ff689
HectorGinory <hectorginorydevs@gmail.com>
2023-05-21 14:04:21 +0200

    Home page finished and readme started

---
+++ README.md
@@ -0,0 +1,39 @@
+# DENTAL CLINIC #
+
+**Author:** Héctor Ginory
+
+**Tecnologies:** ReactJS, HTML, CSS3, JavaScript - ES6, Git
+
+<a href="">Click here to see the site</a>
+
+## CHALLENGE ##
+The chanllenge was to connect create a Frontend to our previous proyect, the backend of a dental clinic.
+<br>
+<a href="https://github.com/HectorGinory/Backend_ClinicaDental">Click here to see the repository</a>
+<br>
+<br>
+Taking into account the requirements, we can advise on the views
+necessary for the proper functioning of the application.
+- User Registration.
+<br>
+*PONER IMAGEN O GIF AQUI*
+
+- User login.
+<br>
+*PONER IMAGEN O GIF AQUI*
+- User profile.
+<br>
+*PONER IMAGEN O GIF AQUI*
+- Appointment search / creation / modification panel.
+<br>
+*PONER IMAGEN O GIF AQUI*
+- View of appointments that I have as a client (only my own).
+<br>
+*PONER IMAGEN O GIF AQUI*
+- View of all existing appointments (as a dentist).
+<br>
+*PONER IMAGEN O GIF AQUI*
+- View of all users registered on the platform (such as
+administrator).
+<br>
+*PONER IMAGEN O GIF AQUI*
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
-import HomeGalerry from '../../common/HomeGallery/HomeGalerry'
-
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, userData } from "../../pages/userSlice";
+import './Home.css'
+import Logo from '../../assets/tooth.png'
 const Home = () => {
-
+  const navigate = useNavigate();
+  const userRdxData = useSelector(userData);
+  const dispatch = useDispatch();
   return (
     <>
-      <div className='body flex-c-c' >
-      
+      <div className='home-container flex-c-c container'>
+        <div className='text-container'>
+          <h1>Dental Clinic</h1>
+          <h2>Your trusted dentist</h2>
+          <p>
+            We are a dental clinic in Madrid that offers a medical team of specialized 
+            dentists and a dental clinic with the most cutting-edge technology in the sector. We 
+            intend to combine science, technology, personal treatment and price. The treatments 
+            that we carry out in the clinic are always carried out by qualified dentists and experts
+            in the field, seeking the treatment that best suits each patient.
+          </p>
+          {userRdxData.credentials.token ? 
+          <button onClick={()=>navigate("/newquote")} className='btn'>Create Quote</button> :
+          <>
+          <button onClick={()=>navigate("/login")} className='btn'>Login</button>
+          <button onClick={()=>navigate("/register")} className='btn'>Register</button>
+          </>}
+        </div>
+        <div className='image-container flex-c-c'>
+          <img src={Logo} alt="clinicial dentist photo" className='image'/>
+        </div>
       </div>
     </>
   )
